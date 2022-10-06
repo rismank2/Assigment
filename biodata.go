ff43599121241ded7d796bd119900774b8520293
Risman Kurniadi <rismankurniadi2@gmail.com>
2022-10-06 17:18:33 +0700

    menghapus data

--- biodata.go
+++ biodata.go
@@ -26,7 +26,6 @@ var Datas = []data{
 	{no_absen: 8, nama: "Fariz Dandy Lazuardi"},
 	{no_absen: 9, nama: "Saritalia"},
 	{no_absen: 10, nama: "Bima Karo Karo"},
-	{no_absen: 10, nama: "Nadine"},
 }
 
 func view(data) {
